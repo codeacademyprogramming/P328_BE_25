48d50f77c625742a19144e39f2baf811480a1be7
Elchin Aghayev <Elchin@code.local>
2023-05-31 21:39:16 +0400

    project added

---
+++ ChatApp/ChatApp/wwwroot/js/chat.js
@@ -0,0 +1,16 @@
+﻿var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+connection.start();
+
+$(document).on("click", "#sendButton", function (e) {
+    let user = $("#userInput").val();
+    let message = $("#messageInput").val();
+
+    connection.invoke("SendMessage", user, message)
+})
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    li.textContent = `${user}: ${message}`;
+})
